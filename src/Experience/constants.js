5f3dd78e1ad7253316ba6c1d23eacd03925ae929
Muhammad Nouval Ar-Rizqy <nouvalrizqy05@gmail.com>
2026-01-21 21:47:12 +0700

    Update constants.js

--- src/Experience/constants.js
+++ src/Experience/constants.js
@@ -11,9 +11,14 @@ import {
 export const ARCADE_SCREEN_WIDTH = 1006.986;
 export const ARCADE_SCREEN_HEIGHT = 1210.1182617331252;
 export const ARCADE_CSS_OBJECT_SCALE = new Vector3(0.00102, 0.00102, 0.00102);
-export const ARCADE_CSS_OBJECT_POSITION = new Vector3(3.24776, 2.7421, 2.3009);
+
+// REVISI: Nilai X digeser sedikit ke arah luar (3.25776) 
+// agar tidak terjadi z-fighting/kedipan dengan mesh monitor
+export const ARCADE_CSS_OBJECT_POSITION = new Vector3(3.25776, 2.7421, 2.3009);
+
 export const ARCADE_CSS_OBJECT_ROTATION_X = -Math.PI / 7;
 export const ARCADE_CSS_OBJECT_ROTATION_Y = -Math.PI / 2;
+
 export const CRT_UNIFORMS = {
   uCurvature: new Vector2(3, 3),
   uScreenResolution: new Vector2(
@@ -27,8 +32,10 @@ export const CRT_UNIFORMS = {
   uBrightness: 2.5,
   uVignetteRoundness: 1,
 };
+
 export const ARCADE_IFRAME_SRC = "https://nouval-drivemad.netlify.app/";
 export const ARCADE_IFRAME_PADDING = "16px";
+
 export const ARCADE_MACHINE_CAMERA_POSITION = new Vector3(-1.7, 5.5, 2.3009);
 export const ARCADE_MACHINE_CAMERA_QUATERNION = new Quaternion(
   -0.17756084520729903,
@@ -52,7 +59,7 @@ export const CAMERA_QUATERNION = new Quaternion(
 );
 export const CAMERA_TARGET = new Vector3(0, 2.5, 0);
 
-//Carpet
+// Carpet
 export const CARPET_SHELLCOUNT = 32;
 export const CARPET_UNIFORMS = {
   uColor: new Color(0.7529412, 0.5424671, 0.4392157).convertSRGBToLinear(),
@@ -65,11 +72,11 @@ export const CARPET_GROUP_SCALE = new Vector3(0.0355, 0.0355, 0.0355);
 export const CARPET_GROUP_POSITION = new Vector3(-2.61408, 0.377, -0.904327);
 export const CARPET_GROUP_ROTATION = new Euler(-Math.PI / 2, 0, 0);
 
-//  Coffee
+// Coffee
 export const COFFEE_GEOMETRY = new PlaneGeometry(0.15, 0.6, 16, 64);
 export const COFFEE_POSITION = new Vector3(0.230979, 2.3, -3.64951);
 
-//Confetti
+// Confetti
 export const CONFETTI_AMOUNT = 500;
 export const CONFETTI_EXPLOSION_RADIUS = 300;
 export const CONFETTI_COLORS = [
@@ -108,7 +115,6 @@ export const RIGHT_MONITOR_CSS_OBJECT_POSITION = new Vector3(
   2.50716,
   -4.14566
 );
-
 export const RIGHT_MONITOR_CSS_OBJECT_SCALE = new Vector3(0.00102, 0.00102, 1);
 export const RIGHT_MONITOR_CSS_OBJECT_ROTATION_Y = (-7.406 * Math.PI) / 180;
 export const RIGHT_MONITOR_IFRAME_SRC = "https://nouval-art-gallery.vercel.app";
@@ -144,6 +150,7 @@ export const ELEMENTS_TO_RAYCAST = [
   "whiteboard",
   "whiteboardCanvas",
 ];
+
 export const ORBIT_CONTROLS_CONFIG = {
   enabled: false,
   screenSpacePanning: true,
@@ -171,8 +178,7 @@ export const WHITEBOARD_CAMERA_QUATERNION = new Quaternion(
   -0.08802977047890838,
   0,
   0,
-  0.9961178441878403,
-  1
+  0.9961178441878403
 );
 export const WHITEBOARD_CAMERA_TARGET = new Vector3(-3.3927, 3.18774, -4.61366);
 
