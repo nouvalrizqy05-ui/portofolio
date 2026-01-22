afa4e4a22d1f27a8d1cd02168c3fed26e7a5f6ab
Muhammad Nouval Ar-Rizqy <nouvalrizqy05@gmail.com>
2026-01-22 21:52:32 +0700

    Update ArcadeScreen.js

--- src/Experience/ArcadeScreen.js
+++ src/Experience/ArcadeScreen.js
@@ -1,12 +1,26 @@
-import { Vector2, ShaderMaterial, DoubleSide, NoBlending, PlaneGeometry, Mesh } from "three";
+mport {
+  Vector2,
+  ShaderMaterial,
+  DoubleSide,
+  NoBlending,
+  PlaneGeometry,
+  Mesh,
+} from "three";
+
 import Experience from "./Experience.js";
 import { CSS3DObject } from "three/examples/jsm/renderers/CSS3DRenderer.js";
 import fragmentShader from "./shaders/screenEffect/fragment.glsl";
 import vertexShader from "./shaders/screenEffect/vertex.glsl";
 import {
-  ARCADE_SCREEN_WIDTH, ARCADE_SCREEN_HEIGHT, ARCADE_CSS_OBJECT_SCALE,
-  ARCADE_CSS_OBJECT_POSITION, ARCADE_CSS_OBJECT_ROTATION_X,
-  ARCADE_CSS_OBJECT_ROTATION_Y, CRT_UNIFORMS, ARCADE_IFRAME_SRC, ARCADE_IFRAME_PADDING
+  ARCADE_SCREEN_WIDTH,
+  ARCADE_SCREEN_HEIGHT,
+  ARCADE_CSS_OBJECT_SCALE,
+  ARCADE_CSS_OBJECT_POSITION,
+  ARCADE_CSS_OBJECT_ROTATION_X,
+  ARCADE_CSS_OBJECT_ROTATION_Y,
+  CRT_UNIFORMS,
+  ARCADE_IFRAME_SRC,
+  ARCADE_IFRAME_PADDING,
 } from "./constants.js";
 
 export default class ArcadeScreen {
@@ -17,9 +31,11 @@ export default class ArcadeScreen {
     this.cssArcadeMachineScene = this.experience.cssArcadeMachineScene;
     this.scene = this.experience.scene;
     this.resources = this.experience.resources;
+    this.mouse = this.experience.mouse;
     this.screenSize = new Vector2(ARCADE_SCREEN_WIDTH, ARCADE_SCREEN_HEIGHT);
     this.model = {};
     this.arcadeMachineMaterial = this.experience.world.baked.model.material2;
+    this.maxAnisotropy = this.renderer.capabilities.getMaxAnisotropy();
     this.audioManager = this.experience.world.audioManager;
     this.setModel();
     this.setArcadeScreen();
@@ -27,7 +43,11 @@ export default class ArcadeScreen {
 
   setModel = () => {
     this.model.arcadeMachineModel = this.resources.items.arcadeMachine.scene;
-    this.model.arcadeMachineModel.traverse((child) => { if (child.isMesh) child.material = this.arcadeMachineMaterial; });
+    this.model.arcadeMachineModel.traverse((child) => {
+      if (child.isMesh) {
+        child.material = this.arcadeMachineMaterial;
+      }
+    });
     this.model.arcadeMachineModel.name = "arcadeMachine";
     this.scene.add(this.model.arcadeMachineModel);
   };
@@ -38,55 +58,96 @@ export default class ArcadeScreen {
     container.style.height = this.screenSize.height + "px";
 
     const iframe = document.createElement("iframe");
+
     iframe.src = ARCADE_IFRAME_SRC;
     iframe.style.width = this.screenSize.width + "px";
     iframe.style.height = this.screenSize.height + "px";
     iframe.style.padding = ARCADE_IFRAME_PADDING;
-    iframe.style.border = "0px";
-    iframe.id = "arcade-screen";
-    iframe.setAttribute('allow', 'autoplay; fullscreen; pointer-lock; keyboard');
 
+    iframe.style.transparent = true;
+    iframe.id = "arcade-screen";
+    iframe.style.boxSizing = "border-box";
+    iframe.style.background = "black";
     container.appendChild(iframe);
-    this.iframe = iframe;
-    iframe.addEventListener("load", () => { this.iframeWindow = iframe.contentWindow; });
+    iframe.addEventListener("load", () => {
+      this.iframeWindow = iframe.contentWindow;
+    });
 
     const css3dobject = new CSS3DObject(container);
+
     css3dobject.scale.copy(ARCADE_CSS_OBJECT_SCALE);
     css3dobject.position.copy(ARCADE_CSS_OBJECT_POSITION);
     css3dobject.rotateY(ARCADE_CSS_OBJECT_ROTATION_Y);
     css3dobject.rotateX(ARCADE_CSS_OBJECT_ROTATION_X);
     this.cssArcadeMachineScene.add(css3dobject);
-
     const materialCRT = new ShaderMaterial({
-      blending: NoBlending, side: DoubleSide,
+      blending: NoBlending,
+      side: DoubleSide,
       uniforms: {
         uCurvature: { value: CRT_UNIFORMS.uCurvature },
-        uScreenResolution: { value: CRT_UNIFORMS.uScreenResolution },
-        uScanLineOpacity: { value: CRT_UNIFORMS.uScanLineOpacity },
-        uBaseColor: { value: CRT_UNIFORMS.uBaseColor },
-        uColor: { value: CRT_UNIFORMS.uColor },
-        uVignetteOpacity: { value: CRT_UNIFORMS.uVignetteOpacity },
+        uScreenResolution: {
+          value: CRT_UNIFORMS.uScreenResolution,
+        },
+        uScanLineOpacity: {
+          value: CRT_UNIFORMS.uScanLineOpacity,
+        },
+        uBaseColor: {
+          value: CRT_UNIFORMS.uBaseColor,
+        },
+        uColor: {
+          value: CRT_UNIFORMS.uColor,
+        },
+        uVignetteOpacity: {
+          value: CRT_UNIFORMS.uVignetteOpacity,
+        },
         uBrightness: { value: CRT_UNIFORMS.uBrightness },
-        uVignetteRoundness: { value: CRT_UNIFORMS.uVignetteOpacity },
+        uVignetteRoundness: {
+          value: CRT_UNIFORMS.uVignetteOpacity,
+        },
       },
-      vertexShader, fragmentShader,
+      vertexShader: vertexShader,
+      fragmentShader: fragmentShader,
     });
+    // Create plane geometry
+    const geometry = new PlaneGeometry(
+      this.screenSize.width,
+      this.screenSize.height
+    );
+    // Create the GL plane mesh
+    this.model.screen = new Mesh(geometry, materialCRT);
 
-    this.model.screen = new Mesh(new PlaneGeometry(this.screenSize.width, this.screenSize.height), materialCRT);
+    // Copy the position, rotation and scale of the CSS plane to the GL plane
     this.model.screen.position.copy(css3dobject.position);
     this.model.screen.rotation.copy(css3dobject.rotation);
     this.model.screen.scale.copy(css3dobject.scale);
     this.model.screen.name = "arcadeMachineScreen";
+
+    // Add to gl scene
     this.model.arcadeMachineModel.add(this.model.screen);
   };
 
+  handleKeyDownParent = (event) => {
+    this.iframeWindow.postMessage(
+      { type: "keyDownParent", key: event.key },
+      "*"
+    );
+  };
+
+  handleKeyUpParent = (event) => {
+    this.iframeWindow.postMessage({ type: "keyUpParent", key: event.key }, "*");
+  };
+
   onMouseMove = () => {
-    const isOverArcade = (this.experience.world.navigation?.objectRaycasted?.object?.name === "arcadeMachineScreen");
-    if (isOverArcade) {
+    if (
+      this.objectRaycasted &&
+      this.objectRaycasted.object &&
+      this.objectRaycasted.object.name == "arcadeMachine"
+    ) {
+      this.experience.navigation.orbitControls.enableDamping = false;
       this.experience.navigation.orbitControls.enabled = false;
       this.webglElement.style.pointerEvents = "none";
-      this.iframe.focus(); 
     } else {
+      this.experience.navigation.orbitControls.enableDamping = true;
       this.experience.navigation.orbitControls.enabled = true;
       this.webglElement.style.pointerEvents = "auto";
     }
@@ -97,20 +158,24 @@ export default class ArcadeScreen {
     window.addEventListener("keyup", this.handleKeyUpParent);
     window.addEventListener("pointermove", this.onMouseMove);
     window.addEventListener("message", this.receiveMessage, false);
-    
-    // Paksa fokus saat layar diklik
-    this.webglElement.addEventListener("mousedown", () => {
-        if (this.experience.world.navigation?.objectRaycasted?.object?.name === "arcadeMachineScreen") {
-            this.iframe.focus();
-        }
-    });
+    this.onMouseMove();
   };
-
-  handleKeyDownParent = (e) => { if (this.iframeWindow) this.iframeWindow.postMessage({ type: "keyDownParent", key: e.key }, "*"); };
-  handleKeyUpParent = (e) => { if (this.iframeWindow) this.iframeWindow.postMessage({ type: "keyUpParent", key: e.key }, "*"); };
-  
-  receiveMessage = (e) => {
-    const audios = ["hit", "tetris", "die", "select1", "select2"];
-    if (audios.includes(e.data)) this.audioManager.playSingleAudio(e.data, 1);
+  deactivateControls = () => {
+    window.removeEventListener("keydown", this.handleKeyDownParent);
+    window.removeEventListener("keyup", this.handleKeyUpParent);
+    window.removeEventListener("pointermove", this.onMouseMove);
+    window.removeEventListener("message", this.receiveMessage, false);
   };
-}
+  receiveMessage = (event) => {
+    if (event.data == "hit") {
+      this.audioManager.playSingleAudio("hit", 1);
+    } else if (event.data == "tetris") {
+      this.audioManager.playSingleAudio("tetris", 1);
+    } else if (event.data == "die") {
+      this.audioManager.playSingleAudio("die", 1);
+    } else if (event.data == "select1") {
+      this.audioManager.playSingleAudio("select1", 1);
+    } else if (event.data == "select2") {
+      this.audioManager.playSingleAudio("select2", 1);
+    }
+  };
