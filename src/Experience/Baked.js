72ec99ed376c367c4a4b6664699ed3246aa025fc
Muhammad Nouval Ar-Rizqy <nouvalrizqy05@gmail.com>
2026-02-04 01:40:10 +0700

    Update Baked.js

--- src/Experience/Baked.js
+++ src/Experience/Baked.js
@@ -1,4 +1,4 @@
-import { Mesh, MeshBasicMaterial, MeshBasicMaterial, PlaneGeometry, SRGBColorSpace, Color, DoubleSide } from "three";
+import { Mesh, MeshBasicMaterial, MeshStandardMaterial, PlaneGeometry, SRGBColorSpace, Color, DoubleSide } from "three";
 import Experience from "./Experience.js";
 
 export default class Baked {
@@ -29,7 +29,7 @@ export default class Baked {
   setModels = () => {
     this.model = {};
 
-    // Load Textures
+    // 1. Load Textures Ruangan
     this.bakedTexture1 = this.configureTexture(this.resources.items.baked1);
     this.model.material = new MeshBasicMaterial({ map: this.bakedTexture1 });
 
@@ -39,16 +39,16 @@ export default class Baked {
     this.bakedTexture3 = this.configureTexture(this.resources.items.baked3);
     this.model.material3 = new MeshBasicMaterial({ map: this.bakedTexture3 });
 
-    // Material Ungu Neon untuk Arcade & Sosmed
+    // 2. Material Ungu Neon untuk Arcade & Sosmed
     this.purpleNeonMaterial = this.model.material3.clone();
     this.purpleNeonMaterial.color = new Color('#8A2BE2');
 
-    // Room Models
+    // 3. Load Model Ruangan
     this.model.room1 = this.resources.items._roomModel.scene;
     this.model.room2 = this.resources.items._roomModel2.scene;
     this.model.room3 = this.resources.items._roomModel3.scene;
 
-    // Arcade Machine
+    // 4. Konfigurasi Arcade Machine
     if (this.resources.items.arcadeMachine) {
         this.model.arcade = this.resources.items.arcadeMachine.scene;
         this.model.arcade.name = "arcadeMachine"; 
@@ -61,7 +61,7 @@ export default class Baked {
         const photoTexture = this.resources.items.myPhoto;
         photoTexture.colorSpace = SRGBColorSpace;
 
-        // 1. Bingkai Glow (Bagian belakang yang menyala)
+        // Bingkai Glow (Bagian belakang yang menyala menggunakan MeshBasicMaterial)
         const frameGlowMaterial = new MeshBasicMaterial({ 
             color: new Color('#8A2BE2'),
             side: DoubleSide,
@@ -70,43 +70,38 @@ export default class Baked {
         });
         this.model.photoFrameBack = new Mesh(new PlaneGeometry(0.56, 0.56), frameGlowMaterial);
 
-        // 2. Foto Utama (Menggunakan MeshBasicMaterial agar bisa Emissive)
-        const photoMaterial = new MeshBasicMaterial({
+        // Foto Utama (Menggunakan MeshStandardMaterial agar mendukung fitur emissive/cahaya)
+        const photoMaterial = new MeshStandardMaterial({
             map: photoTexture,
-          side: DoubleSide,
             emissive: new Color('#8A2BE2'),
-            emissiveIntensity: 0.4,
+            emissiveIntensity: 0.5,
             side: DoubleSide
         });
         this.model.photoFrame = new Mesh(new PlaneGeometry(0.5, 0.5), photoMaterial);
 
-        // Atur Posisi (Gantikan koordinat itchio lama - contoh di tembok dekat meja)
-        const posX = 1.45, posY = 1.2, posZ = -1.85;
+        // Atur Posisi (Z dimajukan ke -1.85 agar tidak 'flicker' atau tertutup tembok)
+        const posX = 1.45, posY = 1.2, posZ = -1.85; 
         this.model.photoFrame.position.set(posX, posY, posZ);
         this.model.photoFrameBack.position.set(posX, posY, posZ - 0.01);
-        
-        // Sesuaikan rotasi jika perlu
-        // this.model.photoFrame.rotation.y = Math.PI * 0.5;
-        // this.model.photoFrameBack.rotation.y = Math.PI * 0.5;
 
-        this.model.photoFrame.name = "photoFrame"; // Untuk Raycaster
+        this.model.photoFrame.name = "photoFrame"; 
         this.scene.add(this.model.photoFrame, this.model.photoFrameBack);
     }
 
-    // Social Media Icons
+    // 5. Konfigurasi Ikon Sosial
     this.model.linkedin = this.resources.items.linkedin.scene;
     this.model.linkedin.name = "linkedin";
     this.model.github = this.resources.items.github.scene;
     this.model.github.name = "github";
 
-    // Apply Materials
+    // 6. Apply Materials
     this.setMaterial(this.model.room1, this.model.material);
     this.setMaterial(this.model.room2, this.model.material2);
     this.setMaterial(this.model.room3, this.model.material3);
     this.setMaterial(this.model.linkedin, this.purpleNeonMaterial);
     this.setMaterial(this.model.github, this.purpleNeonMaterial);
 
-    // Add to Scene
+    // 7. Add to Scene
     this.scene.add(this.model.room1, this.model.room2, this.model.room3, this.model.linkedin, this.model.github);
   };
 }
